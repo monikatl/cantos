0eb6806e7290be5b44d413c7749eee8865727d1a
monikatl <monika.joanna.baszczyk@gmail.com>
2025-01-11 18:54:09 +0100

    Calendar view

--- app/src/main/java/com/example/singandsongs/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/singandsongs/ui/calendar/CalendarFragment.kt
@@ -1,8 +1,29 @@
 package com.example.singandsongs.ui.calendar
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.singandsongs.databinding.FragmentCalendarBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CalendarFragment : Fragment() {
+
+  private lateinit var binding: FragmentCalendarBinding
+  private val viewModel: CalendarViewModel by viewModels()
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+
+    binding = FragmentCalendarBinding.inflate(inflater, container, false)
+    binding.viewModel = viewModel
+
+
+    return binding.root
+  }
 }
